90600b32f6bfb80bf5a7574e30dc4fecb44c3cf2
minecraft-Yang458 <18457376055@163.com>
2026-02-18 12:08:19 +0800

    把我添入

--- src/data/contributors.ts
+++ src/data/contributors.ts
@@ -133,6 +133,12 @@ export const contributors: Contributor[] = [
     avatar: "https://github.com/xuezhaju/xuezhaju_Icon/blob/main/F421312221AA32EA130B490230A78779.jpg?raw=true",
     url: "https://github.com/xuezhaju",
   },
+  {
+  name: "Yang458"; // 名字或昵称
+  role: "修订部分文档"; // 角色描述
+  avatar: "https://api.rms.net.cn/head/Yang4893"; // 头像 URL
+  url: "https://github.com/minecraft-Yang458" ; // 可选：个人主页链接
+  },
   // ============================================
   // 在这里添加更多贡献者！
   // ============================================
